99941dbe848a14576e89be3f121212f30fb7f320
AriiSharma <ariisharma47@gmail.com>
2025-02-27 14:41:45 -0700

    updating the front end for navbar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,17 @@
+import Featured from "@/component/featured/Featured";
 import styles from "./homepage.module.css"; 
+import CategoryList from "@/component/categoryList/CategoryList";
+import CardList from "@/component/cardList/CardList";
+import Menu from "@/component/Menu/Menu";
 
 
 export default function Home() {
-  return <div>
-
-    </div>;
+  return <div className = {styles.container}>
+    <Featured />
+    <CategoryList />
+    <div className = {styles.container}>
+      <CardList />
+      <Menu />
+    </div>
+    </div>
 }
--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import styles from "./navbar.module.css"
+import styles from './navbar.module.css';
+import Link from 'next/link';
+import AuthLinks from '../authLinks/AuthLinks';
+import ThemeToggle from '../themeToggle/ThemeToggle';
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
-    <div className = {styles.container}> Navbar </div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.social}>
+        <Image src="/youtube.png" alt="youtube" width={28} height={28} />
+        <Image src="/instagram.png" alt="instagram" width={24} height={24} />
+        <Image src="/linkedin.png" alt="linkedin" width={28} height={28} />
+        <Image src="/github.png" alt="github" width={28} height={28} />
+      </div>
 
-export default Navbar
+      <div className={styles.logo}>Guff Blog</div>
+
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link href="/">Homepage</Link>
+        <Link href="/">Contacts</Link>
+        <Link href="/">About</Link>
+        <AuthLinks />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
