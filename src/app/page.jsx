dacb343300729ac1978b8545934843cf9e8ecf11
AriiSharma <ariisharma47@gmail.com>
2025-02-27 10:08:57 -0700

    Created basic front end file components
    Created basic file components for the blog application such  as  navbar, menu, footer, category list, etc.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,8 @@
-import styles from "./homepage.module.css";
+import styles from "./homepage.module.css"; 
+
 
 export default function Home() {
   return <div>
-    <link href ="/">
-    </link> 
-    <h1> Lorem ipsum odor amet, consectetuer adipiscing elit. Posuere pellentesque hendrerit nunc facilisi vel. </h1>
-    Lorem ipsum odor amet, consectetuer adipiscing elit. Commodo fringilla sodales ad elit senectus. Lacinia nunc dignissim natoque dictumst maximus molestie fames. Lectus nunc enim elementum class tellus enim arcu congue. Mollis feugiat justo mauris vehicula etiam ut nullam. Scelerisque condimentum nulla tincidunt rhoncus proin risus penatibus sed. Lacus mi fusce diam torquent ullamcorper mollis. Volutpat aenean ultrices sapien himenaeos hendrerit odio bibendum convallis quam.
-
-Luctus primis mi; tempor sed facilisi parturient dignissim. Eu eleifend blandit nibh platea nullam feugiat. Etiam justo penatibus montes curabitur molestie facilisi augue. Lectus velit fusce ex quam; adipiscing sit nullam lorem. Duis dignissim torquent, purus varius interdum ultrices. Ut consectetur lobortis nullam nascetur aliquet rhoncus phasellus sollicitudin. Inceptos sapien litora lobortis in nullam dolor pretium duis luctus. Curae fames ullamcorper vehicula, vel elit scelerisque metus mi. Neque ac urna primis porttitor, curae integer rutrum.
-
-Consequat imperdiet orci dis pharetra sagittis quam venenatis proin taciti? Viverra hac potenti fringilla sem magna eros egestas. Eu malesuada adipiscing magna eleifend efficitur blandit nisl blandit? Fringilla quisque ullamcorper pellentesque feugiat taciti neque accumsan per. Ultricies fames ac vivamus tempus dui primis. Tortor finibus fringilla sollicitudin fames malesuada mus vulputate tempus?
 
     </div>;
 }
