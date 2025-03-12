cf642b8c3992d9a7e50238fb966a8b2fb9a36afe
AriiSharma <ariisharma47@gmail.com>
2025-03-11 20:09:27 -0600

    Created comments router js to add endpoints from prisma to show & add newer comments
    Created endpoint to directly get messages from prisma and to add newer comments using comments router postSlug.

---
+++ src/app/api/comments/route.js
@@ -0,0 +1,57 @@
+import { NextResponse } from 'next/server';
+import prisma from '@/utils/connect';
+import { getAuthSession } from '@/utils/auth';
+
+//get all comments of post
+
+export const GET = async (req) => {
+  const { searchParams } = new URL(req.url);
+
+  console.log(searchParams)
+
+  const postSlug = searchParams.get("postSlug");
+
+  try {
+    const comments = await prisma.comment.findMany({
+      where: { postSlug },
+      include: { user: true },
+    });
+
+    console.log("Fetched post:", comments);
+
+    return new NextResponse(JSON.stringify( comments, { status: 200 }));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: 'Something went wrong!' }),
+      { status: 500 }
+    );
+  }
+};
+
+//create another endpoint to add comments
+
+export const POST = async (req) => {
+  const session = await getAuthSession();
+
+  if(!session) {
+    return new NextResponse(
+      JSON.stringify({ message: "Not Authenticated"}, {status: 401})
+    )
+  }
+
+  try {
+    const body = await req.json();
+    const comment = await prisma.comment.create({
+      data: { ...body, userEmail: session.user.email },
+    })
+
+    return new NextResponse(JSON.stringify( comment, { status: 200 }));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: 'Something went wrong!' }),
+      { status: 500 }
+    );
+  }
+};
