a0200856cf1961ef8d9e56cf9207c02e8f982ec3
AriiSharma <ariisharma47@gmail.com>
2025-03-24 11:01:24 -0600

    .

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,10 @@
-export const baseUrl = process.env.NODE_ENV === 'production' 
-  ? process.env.NEXTAUTH_URL // Use the environment variable for production
-  : 'http://localhost:3000';
+ // src/utils/api.js
+ export const baseUrl = process.env.NODE_ENV === 'production' 
+ ? '' // Empty string for relative URLs in production
+ : 'http://localhost:3000';
 
 export const fetcher = async (url) => {
-  const res = await fetch(`${baseUrl}${url}`); // Use the base URL with the endpoint
-  if (!res.ok) throw new Error('An error occurred while fetching the data.');
-  return res.json();
-};
+ const res = await fetch(url);
+ if (!res.ok) throw new Error('An error occurred while fetching the data.');
+ return res.json();
+};  
